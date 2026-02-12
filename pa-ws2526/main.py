57666a025278634e8e113bb8da02df37282855e3
FruzFraz <83365906+FruzFraz@users.noreply.github.com>
2026-02-12 11:30:52 +0100

    0.8.2
    adding processed_data.loc
    corected end

--- pa-ws2526/main.py
+++ pa-ws2526/main.py
@@ -48,57 +48,72 @@ def main():
         group_service_loss = []
         group_power = []
 
-    for run in range(1, 11):
-        run_id = f"run_{run:02d}"
-        base = f"{group}/{run_id}"
+        for run in range(1, 11):
+            run_id = f"run_{run:02d}"
+            base = f"{group}/{run_id}"
 
-        start_time_index = fn.read_metadata(
-            file_path, base, "analyse_start_time_index"
-        )
+            start_time_index = fn.read_metadata(
+                file_path, base, "analyse_start_time_index"
+            )
 
-        tank_pressure = fn.read_data(file_path, f"{base}/tank_1_pressure")
-        power_1 = fn.read_data(file_path, f"{base}/pump_1_power")
-        power_2 = fn.read_data(file_path, f"{base}/pump_2_power")
-        time = fn.read_data(file_path, f"{base}/time")
+            tank_pressure = fn.read_data(file_path, f"{base}/tank_1_pressure")
+            power_1 = fn.read_data(file_path, f"{base}/pump_1_power")
+            power_2 = fn.read_data(file_path, f"{base}/pump_2_power")
+            time = fn.read_data(file_path, f"{base}/time")
 
     
 
 
-    service_fill = fn.cap_service_data(tank_pressure, setpoint)
-    if not fn.check_negative_values(power_1) or not fn.check_negative_values(power_2):
-        print(f"Warning: Negative power values in {group} {run_id}")
-    sf_int = fn.integral_with_time_step(
-        service_fill[start_time_index:], time[start_time_index:]
-    )
+            service_fill = fn.cap_service_data(tank_pressure, setpoint)
+            if not fn.check_negative_values(power_1) or not fn.check_negative_values(power_2):
+                print(f"Warning: Negative power values in {group} {run_id}")
+            sf_int = fn.integral_with_time_step(
+                service_fill[start_time_index:], time[start_time_index:]
+            )
 
-    target = np.full_like(service_fill[start_time_index:], setpoint)
-    st_int = fn.integral_with_time_step(target, time[start_time_index:])
+            target = np.full_like(service_fill[start_time_index:], setpoint)
+            st_int = fn.integral_with_time_step(target, time[start_time_index:])
 
-    service_loss = fn.calculate_service_loss(sf_int, st_int)
+            service_loss = fn.calculate_service_loss(sf_int, st_int)
 
-    power_int = (
-    fn.integral_with_time_step(power_1, time)
-    + fn.integral_with_time_step(power_2, time)
-    )
+            power_int = (
+            fn.integral_with_time_step(power_1, time)
+            + fn.integral_with_time_step(power_2, time)
+            )
 
-    power_Wh = fn.convert_Ws_to_Wh(power_int)
+            power_Wh = fn.convert_Ws_to_Wh(power_int)
 
-    group_service_loss.append(service_loss)
-    group_power.append(power_Wh)
+            group_service_loss.append(service_loss)
+            group_power.append(power_Wh)
     sl_mean, sl_std = fn.calculate_mean_and_std(group_service_loss)
     p_mean, p_std = fn.calculate_mean_and_std(group_power)
+
+    processed_data.loc[group] = [p_mean, p_std, sl_mean, sl_std]   
+    print(processed_data)
    
-   fn.save_dataframe_in_hdf5_with_metadata(
-        prcoessed_data,
-        data_archve_path,
-        Matadata_path,
-   )
-    processed_data.loc[group] = [p_mean, p_std, sl_mean, sl_std]
-    fig = fn.plot_service_loss_vs_power(
-        plot_data,
-        plot_format_data,
+    fn.save_dataframe_in_hdf5_with_metadata(
+            processed_data,
+            data_archive_path,
+            "plot_data",
+            metadata_dict,
+    )
+    plot_data, plot_format_data = fn.read_plot_data(
+        data_archive_path, "plotdata"
+    )
 
+     
+    fig = fn.plot_service_loss_vs_power(plot_data, plot_format_data)
 
+    
+    fn.publish_plot(
+        fig,
+        source_paths=[
+            data_archive_path,
+            "./functions/functions.py",
+            "./main.py",
+        ],
+        destination_path="./plotid",
+    )
     
 if __name__ == "__main__":
     main()
